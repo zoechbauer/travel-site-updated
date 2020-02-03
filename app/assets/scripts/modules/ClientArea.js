0a5c935a0ea729c7313d51902d8d310b8d8c48f0
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-03 09:48:46 +0100

    Added client area feature

---
+++ app/assets/scripts/modules/ClientArea.js
@@ -0,0 +1,50 @@
+import Axios from 'axios'
+
+class ClientArea {
+    constructor() {
+        this.injectHtml();
+        this.form = document.querySelector('.client-area__form');
+        this.field = document.querySelector('.client-area__input');
+        this.contentArea = document.querySelector('.client-area__content-area');
+        this.events();
+    }
+
+    events() {
+        this.form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.sendRequest();
+        })
+    }
+
+    sendRequest() {
+        const url = 'https://zoe-travel-site.netlify.com/.netlify/functions/secret-area';
+        const data = {password: this.form.value};
+        Axios.post(url, ).then(response => {
+            this.form.remove(),
+            this.contentArea.innerHTML = response.data;
+            console.log(repsonse);
+        }).catch(() => {
+            this.contentArea.innerHTML = `<p class="client-area__error">This secret phrase ist not correct. Try again.</p>`;
+            this.field.value= '';
+            this.field.focus();
+        });
+    }
+
+    injectHtml() {
+        document.body.insertAdjacentHTML('beforeend', `
+        <div class="client-area">
+            <div class="wrapper wrapper--medium">
+            <h2 class="section-title section-title--blue">Secret Client Area</h2>
+            <form class="client-area__form" action="">
+                <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
+                <button class="btn btn--orange">Submit</button>
+            </form>
+            <div class="client-area__content-area"></div>
+            </div>
+        </div>
+        `)
+    }
+
+}
+
+export default ClientArea;

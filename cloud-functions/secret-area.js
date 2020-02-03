3f6253a6c8024d20c59104c44e07e7fad2fabf2e
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-03 10:43:57 +0100

    CORS Solution (Test Cloud Function from Localhost)

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,18 @@
 exports.handler = function(event, context, callback) {
+    // use headers if you want to test the cloud function locally
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+      }
+     
+      if (event.httpMethod !== "POST") {
+        return callback(null, {
+          statusCode: 200,
+          headers,
+          body: "This was not a POST request"
+        })
+      } 
+
     const secretContent = `
         <h3>Welcome To The Secret Area</h3>
         <p>Here we can tell you that the sky is <strong>blue</strong>, and 2 plus 2 equals 4.</p>
@@ -14,11 +28,13 @@ exports.handler = function(event, context, callback) {
     if (body.password == 'javascript') {
         callback(null, {
             statusCode: 200,
+            headers,
             body: secretContent
         });
     } else {
         callback(null, {
-            statusCode: 401
+            statusCode: 401,
+            headers
         });
     }
     
